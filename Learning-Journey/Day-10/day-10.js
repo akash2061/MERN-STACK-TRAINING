bbc1aa480d217f402c57a7ca23e6b6d1645627ab
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-22 13:29:09 +0530

    Day-10

---
+++ Learning-Journey/Day-10/day-10.js
@@ -0,0 +1,14 @@
+let a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let filterArray = a.filter(i => i % 20 == 0);
+console.log(a);
+console.log(filterArray);
+
+let sum = a.reduce((acc, curr) => acc + curr);
+console.log("\n1st sum: " + sum);
+let sum1 = a.reduce((acc, curr) => acc + curr, -250);
+console.log("2nd sum: " + sum1);
+
+let findArray = a.find((i) => i % 3 == 0);
+console.log("\nFind: " + findArray);
+let findin = a.findIndex((i) => i % 3 == 0);
+console.log("Index: " + findin);
