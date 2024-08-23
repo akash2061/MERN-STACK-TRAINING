e68e2c75928842cbf11747ca928fff46817857b4
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-23 19:46:18 +0530

    Javascript task 2

---
+++ Practice-Questions/Task-3/js_task_2.js
@@ -0,0 +1,48 @@
+// String Concatenation:
+// Question 1: String Concatenation
+function getFullName(firstName, lastName) {
+    const fullName = firstName + " " + lastName;
+    console.log(fullName);
+}
+
+// Example usage:
+console.log();
+getFullName("John", "Doe");
+
+
+// If-Else Statements:
+// Question 2: If-Else Statements
+function checkNumber(num) {
+    if (num > 0) {
+        console.log('Positive');
+    } else if (num < 0) {
+        console.log('Negative');
+    } else {
+        console.log('Zero');
+    }
+}
+
+// Example usage:
+console.log();
+checkNumber(5);    // Output: Positive
+checkNumber(-3);   // Output: Negative
+checkNumber(0);    // Output: Zero
+
+
+// Question 3: Loops
+console.log();
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// Question 5: Arrays
+console.log();
+const numbers = [10, 20, 30, 40, 50];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log('Sum of array elements:', sum);
