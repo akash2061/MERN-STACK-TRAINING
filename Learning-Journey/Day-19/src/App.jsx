6ecec6caa5c8a28e4d3ffc5c3b42e2842297e12e
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-03 17:48:47 +0530

    Reducer

---
+++ Learning-Journey/Day-19/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Reducer from './Reducer'
+
+function App() {
+
+  return (
+    <>
+    <div className=' bg-gray-500'>
+      <Reducer/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ Learning-Journey/Day-19/src/Reducer.jsx
@@ -0,0 +1,40 @@
+import React, { useReducer } from 'react'
+
+const Reducer = () => {
+    const initialState = { count: 0, loading: true };
+
+    function countReducer(state, action) {
+        switch (action.type) {
+            case "increment":
+                return { ...state, count: state.count + 1 };
+            case "decrement":
+                return { ...state, count: state.count - 1 };
+            case "reset":
+                return { ...state, count: 0 };
+            case "loading":
+                return { ...state, loading: !state.loading };
+            default:
+                return new Error("Unknown Type");
+        }
+    }
+    const [state, dispatch] = useReducer(countReducer, initialState);
+    return (
+        <div className='p-20 flex flex-col items-center justify-center min-h-screen'>
+            <p className='text-2xl text-white'>
+                {state.count}
+            </p>
+            <br />
+            <div className='p-20 flex justify-around gap-20'>
+                <button className='p-2 bg-gray-200' onClick={() => {
+                    dispatch({ type: "increment" })
+                }}>increment</button>
+                <button className='p-2 bg-gray-200' onClick={() => {
+                    dispatch({ type: "decrement" })
+                }}>decrement</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default Reducer
