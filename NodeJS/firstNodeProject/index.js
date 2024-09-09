e52784d9c49cae129ea3b52de4846beb7fca0c7d
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-09 18:08:53 +0530

    Day-25

---
+++ NodeJS/firstNodeProject/index.js
@@ -0,0 +1,10 @@
+const express = require("express"); //! important
+
+const app = express(); //! important
+
+//! CRUD
+app.
+
+app.listen(3000, ()=>{
+    console.log("Server is running on 3000");
+});
