9245806b7e6665dec102c421771b2e8e3f6dd9a5
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-10 14:10:53 +0530

    get & post

--- NodeJS/Day-25-26/router/user.js
+++ NodeJS/Day-25-26/router/user.js
@@ -1,17 +1,19 @@
 const express = require("express");
-
-const router = express.router;
+const users = require("../user");
+const router = express.Router();
 
 router.get("/users", (req, res) => { //! http://localhost:3000/api/users
-
+    res.send({ message: "User Fetched", data: "users" });
 });
 
-router.post("/createUser", (req, res) => { //! http://localhost:3000/api/createUser
-
+router.post("/users", (req, res) => { //! http://localhost:3000/api/createUser
+    const data = req.body;
+    users.push(data);
+    res.send({ message: "User Created", data: data });
 });
 
 router.put("/updateUser", (req, res) => { //! http://localhost:3000/api/updateUser
-
+    
 });
 
 router.delete("/deleteUser", (req, res) => { //! http://localhost:3000/api/deleteUser
---
+++ NodeJS/Day-25-26/user.js
@@ -0,0 +1,9 @@
+const users = [
+    {
+        username: "Morningstar_2061",
+        email: "foo@google.com",
+        number: "1023456789"
+    }
+]
+
+module.exports = users;
