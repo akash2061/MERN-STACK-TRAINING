e535a948b3997b5cdef44c0942301147ccf265d5
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-11 19:05:19 +0530

    Day-27

--- NodeJS/Day-25-26/controller/user.js
+++ NodeJS/Day-25-26/controller/user.js
@@ -1,21 +1,17 @@
-const express = require("express");
-
-const router = express.Router();
 const users = require("../user");
-const fs = require("fs");
 
-router.get("/users", (req, res) => {            //! http://localhost:3000/api/users
+exports.getUser = (req, res) => {
     res.send({ message: "User Fetched", data: users });
-});
+};
 
-router.post("/users", (req, res) => {      //! http://localhost:3000/api/createUser
+exports.createUser = (req, res) => {
     const data = req.body;
     const modifiedData = { id: users.length + 1, ...data };
     users.push(modifiedData);
     res.send({ message: "User Created", data: data });
-});
+};
 
-router.put("/users/:id", (req, res) => {       //! http://localhost:3000/api/updateUser
+exports.updateUser = (req, res) => {
     // console.log(req.params);
     const { id } = req.params;
     let user = users.find(item => item.id === +id);
@@ -25,9 +21,9 @@ router.put("/users/:id", (req, res) => {       //! http://localhost:3000/api/upd
     user.contactDetails = req.body.contactDetails;
     // user = {...user, ...req, body};
     res.send({ message: "User Updated" });
-});
+};
 
-router.delete("/users/:id", (req, res) => {    //! http://localhost:3000/api/deleteUser
+exports.deleteUser = (req, res) => {
     const { id } = req.params;
     let user = users.find(item => item.id === +id);
     if (!user) {
@@ -35,10 +31,4 @@ router.delete("/users/:id", (req, res) => {    //! http://localhost:3000/api/del
     }
     users.splice(user, 1);
     res.send({ message: "User deleted" });
-});
-
-module.exports = router;
-
-// Model, View, Controller
-
-// index.js >> Router >> Controller >> Model
+};
