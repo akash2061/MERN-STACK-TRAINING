b9477fc265d9962083bf847b288f83cd60abaeab
akash2061 <lucifermorningstar2061@gmail.com>
2024-09-11 13:25:22 +0530

    Day-27

--- NodeJS/Day-25-26/router/user.js
+++ NodeJS/Day-25-26/router/user.js
@@ -1,25 +1,42 @@
 const express = require("express");
-const users = require("../user");
-const router = express.Router();
 
-router.get("/users", (req, res) => { //! http://localhost:3000/api/users
-    res.send({ message: "User Fetched", data: "users" });
+const router = express.Router();
+const users = require("../user");
+
+
+router.get("/users", (req, res) => {            //! http://localhost:3000/api/users
+    res.send({ message: "User Fetched", data: users });
 });
 
-router.post("/users", (req, res) => { //! http://localhost:3000/api/users
+router.post("/users", (req, res) => {      //! http://localhost:3000/api/createUser
     const data = req.body;
-    const modifyData = { id: users.length + 1, ...data };
-    users.push(modifyData);
+    const modifiedData = { id: users.length + 1, ...data };
+    users.push(modifiedData);
     res.send({ message: "User Created", data: data });
 });
 
-router.put("/users/:id", (req, res) => { //! http://localhost:3000/api/users
+router.put("/users/:id", (req, res) => {       //! http://localhost:3000/api/updateUser
     // console.log(req.params);
     const { id } = req.params;
+    let user = users.find(item => item.id === +id);
+    if (!user) {
+        return res.send({ message: "User not found!" });
+    }
+    user.contactDetails = req.body.contactDetails;
+    // user = {...user, ...req, body};
+    res.send({ message: "User Updated" });
 });
 
-router.delete("/users", (req, res) => { //! http://localhost:3000/api/users
-
+router.delete("/users/:id", (req, res) => {    //! http://localhost:3000/api/deleteUser
+    const { id } = req.params;
+    let user = users.find(item => item.id === +id);
+    if (!user) {
+        return res.send({ message: "User not found!" });
+    }
+    users.splice(user, 1);
+    res.send({ message: "User deleted" });
 });
 
 module.exports = router;
+
+// Model, View, Controller
