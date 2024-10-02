360c23a7629a482f6d6cade19658d15db0360bab
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-02 15:06:06 +0530

    NextJS

---
+++ NextJS/next-app/src/app/products/[productId]/page.js
@@ -0,0 +1,5 @@
+export default function ProductDetails({ params }) {
+    return (
+        <h1>Product Details - Page {params.productId}</h1>
+    )
+}
---
+++ NextJS/next-app/src/app/products/page.js
@@ -0,0 +1,7 @@
+export default function Products() {
+    return (
+        <h1>
+            Dashboard-Products-Page
+        </h1>
+    )
+}
