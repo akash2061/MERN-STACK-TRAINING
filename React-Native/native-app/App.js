fc742aee8b70efbddccfd6d68bb970568a501efd
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-04 11:17:43 +0530

    Added Scroll

--- React-Native/native-app/App.js
+++ React-Native/native-app/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Vector from './assets/adaptive-icon.png';
 
 export default function App() {
   return (
-    <View style={{backgroundColor : "black", justifyContent:'center', alignItems:'center', flex:1}}>
-      <Text style={{color:"azure", fontSize:50}}>Hello </Text>
-      <Image source={Vector} style={{height:100, width:100}}/>
+    <View style={{backgroundColor : "black", justifyContent:'center', alignItems:'center', flex:1, paddingTop:30}}>
+      <ScrollView>
+        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+        <ImageBackground source={Vector} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+        <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+      </ScrollView>
     </View>
   );
 }
