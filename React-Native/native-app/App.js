395b4221f7fed06befe94b67a58e4d4dec5d679b
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-07 13:20:27 +0530

    Indent

--- React-Native/native-app/App.js
+++ React-Native/native-app/App.js
@@ -8,28 +8,28 @@ export default function App() {
   return (
     <View style={{backgroundColor : "black", justifyContent:'center', alignItems:'center', flex:1, paddingTop:30}}>
         <ScrollView>
-        <ImageBackground source={Vector} style={{height:200, width:200}}/>
-        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
-        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
-        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
-        <Text style={{color:"azure", fontSize:50}}>Hello </Text>
-        <Button title="Open Modal" color="blue" onPress={() => {setOpen(true)}} />
-        <Modal visible={open} animationType="fade" transparent={true}>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)'}}>
-            <View style={{width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10}}>
-              <Text style={{fontSize: 20, marginBottom: 20}}>This is a modal!</Text>
-              <Button title="Close Modal" color="blue" onPress={() => {setOpen(false)}} />
+          <ImageBackground source={Vector} style={{height:200, width:200}}/>
+          <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+          <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+          <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+          <Text style={{color:"azure", fontSize:50}}>Hello </Text>
+          <Button title="Open Modal" color="blue" onPress={() => {setOpen(true)}} />
+          <Modal visible={open} animationType="fade" transparent={true}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)'}}>
+              <View style={{width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10}}>
+                <Text style={{fontSize: 20, marginBottom: 20}}>This is a modal!</Text>
+                <Button title="Close Modal" color="blue" onPress={() => {setOpen(false)}} />
+              </View>
             </View>
-          </View>
-        </Modal>
-        <Pressable onPress={()=>{alert('Pressable Component')}} style={{ backgroundColor:'midnightblue', marginTop:10}}>
-          <Text style={{color:'white'}}>
-            Pressable Component
-          </Text>
-          <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
-        </Pressable>
-        {/* <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/> */}
-    </ScrollView>
-      </View>
+          </Modal>
+          <Pressable onPress={()=>{alert('Pressable Component')}} style={{ backgroundColor:'midnightblue', marginTop:10}}>
+            <Text style={{color:'white'}}>
+              Pressable Component
+            </Text>
+            <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/>
+          </Pressable>
+          {/* <Image source={{uri:"https://avatars.githubusercontent.com/u/113748706?v=4"}} style={{height:100, width:100}}/> */}
+      </ScrollView>
+    </View>
   );
 }
